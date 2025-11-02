613dfa006ae4b30f20e16f9456382a1767241620
george <mullergeorgey21@gmail.com>
2025-11-02 13:30:26 +0530

    vercel removel

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',  // ← ADD THIS LINE
     rollupOptions: {
       output: {
-        // Add hash to filename to bust cache
         entryFileNames: 'assets/[name].[hash].js',
         chunkFileNames: 'assets/[name].[hash].js',
         assetFileNames: 'assets/[name].[hash].[ext]',
