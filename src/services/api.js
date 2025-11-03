98beffb20c570e4b252e05220f80d04301060cb6
george <mullergeorgey21@gmail.com>
2025-11-03 23:39:50 +0530

    newst commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBase = import.meta.env.VITE_API_URL || 'https://expense-tracker-flax-one-83.vercel.app';
+const apiBase = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
   baseURL: apiBase,
